a333cdf089c88db886214a2a91392e3b0cede20e
Samantha Lugtu <sammylugtu@samanthas-air.mynetworksettings.com>
2024-02-15 16:14:33 -0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import PuppyPal from './PuppyPal'
+
+function App() {
+  return (
+    <>
+      <div>
+        <PuppyPal/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/PuppyPal.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function PuppyPal() {
+
+
+
+
+
+
+  
+}
+
+export default PuppyPal
